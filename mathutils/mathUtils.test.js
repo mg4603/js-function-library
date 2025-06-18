b3a97b2582509a51d6d3bc6d5444449c4d21b954
michael <michael.g.george@gmail.com>
2025-06-19 01:43:22 +0530

    Test factorial fn from mathUtils

--- mathutils/mathUtils.test.js
+++ mathutils/mathUtils.test.js
@@ -1,4 +1,4 @@
-const { squared, cubed } = require("./mathUtils.js");
+const { squared, cubed, factorial } = require("./mathUtils.js");
 
 describe('squared', ()=> {
     test('should return the square of a positive number', () => {
@@ -40,3 +40,25 @@ describe('cubed', ()=> {
 
     })
 });
+
+describe('factorial', () => {
+    test('factorial of 0', () => {
+        expect(factorial(0)).toBe(1);
+    });
+    test('factorial of negative number', () => {
+        expect(factorial(-5)).toBe(undefined);
+    });
+    test('factorial of 1', () => {
+        expect(factorial(1)).toBe(1);
+    });
+    test('factorial of positive integer', () => {
+        expect(factorial(5)).toBe(120);
+    });
+    test('should throw a TypeError when input iis not a number', () => {
+        expect(() => factorial('a')).toThrow(TypeError);
+        expect(() => factorial(null)).toThrow(TypeError);
+        expect(() => factorial(undefined)).toThrow(TypeError);
+        expect(() => factorial([])).toThrow(TypeError);
+        expect(() => factorial({})).toThrow(TypeError);
+    })
+})
