7641d50119ab4592d03252aa88e41db3620df98a
michael <michael.g.george@gmail.com>
2025-06-18 13:41:38 +0530

    Test cubed fn from mathUtils

--- __tests__/mathUtils.test.js
+++ __tests__/mathUtils.test.js
@@ -1,4 +1,4 @@
-const { squared } = require("../src/mathUtils.js");
+const { squared, cubed } = require("../src/mathUtils.js");
 
 describe('squared', ()=> {
     test('should return the square of a positive number', () => {
@@ -16,6 +16,27 @@ describe('squared', ()=> {
         expect(() => squared(undefined)).toThrow(TypeError);
         expect(() => squared([])).toThrow(TypeError);
         expect(() => squared({})).toThrow(TypeError);
+
+    })
+});
+
+
+describe('cubed', ()=> {
+    test('should return the cube of a positive number', () => {
+        expect(cubed(4)).toBe(64);
+    });
+    test('should return the cube of a negative number', () => {
+        expect(cubed(-4)).toBe(-64);
+    });
+    test('should return 0 when input is 0', () => {
+        expect(cubed(0)).toBe(0);
+    });
+    test('should throw a TypeError when input is not a number', () => {
+        expect(() => cubed('a')).toThrow(TypeError);
+        expect(() => cubed(null)).toThrow(TypeError);
+        expect(() => cubed(undefined)).toThrow(TypeError);
+        expect(() => cubed([])).toThrow(TypeError);
+        expect(() => cubed({})).toThrow(TypeError);
 
     })
 });
